4f1bac28609399803844406f01b18c39a2ea6e2b
georgeygigz <georgeymutti@gmail.com>
2020-08-28 15:54:38 +0300

    clear repo

--- src/components/Profile/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Profile.css";
-const Quotes = () => {
-  return <div className="profile"></div>;
-};
-
-export default Quotes;
--- src/components/Quotes/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Quotes.css";
-import Profile from "../Profile/index";
-
-const Quotes = () => {
-  return <div className="quotes"></div>;
-};
-
-export default Quotes;
