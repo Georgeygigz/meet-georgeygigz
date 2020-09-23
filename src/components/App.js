694f824fb1d63cd03d2a49f708e3aaa1400ddb3d
georgeygigz <georgeymutti@gmail.com>
2020-09-23 15:35:54 +0300

    blog redux

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import PostList from './PostList'
+
+class App extends Component {
+    state = {  }
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <PostList/>
+            </div>
+         );
+    }
+}
+
+export default App;
