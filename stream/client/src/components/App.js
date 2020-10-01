46403473fb400d89fb85abca223ede961359a161
georgeygigz <georgeymutti@gmail.com>
2020-10-01 09:27:34 +0300

    streamy app

---
+++ stream/client/src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+import history from '../history'
+
+class App extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div className="ui container">
+        <Router history={history}>
+          <div>
+            <Header />
+            <Route path="/" exact component={StreamList} />
+            <Route path="/stream/new" exact component={StreamCreate} />
+            <Route path="/stream/edit/:id" exact component={StreamEdit} />
+            <Route path="/stream/delete/:id" exact component={StreamDelete} />
+            <Route path="/stream/:id" exact component={StreamShow} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
